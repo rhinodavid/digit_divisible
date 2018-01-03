a4b0be23f8ccf382f4ace89272b2fecf48dca704
David Walsh <davidwalsh@fb.com>
2018-01-02 19:01:49 -0500

    Create library and main fn

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+pub fn run(x: u32) -> Result<bool, &'static str> {
+    return match x == 0 {
+        false => Ok(make_digits(x).iter().all(|d| is_evenly_divisible(x, *d))),
+        true => Err("This doesn't work with zero!"),
+    };
+}
+
+fn is_evenly_divisible(quotient: u32, divisor: u32) -> bool {
+    return match divisor == 0 {
+        false => quotient % divisor == 0,
+        true => false,
+    };
+}
+
+fn make_digits(mut x: u32) -> Vec<u32> {
+    let mut result = vec![];
+    if x == 0 {
+        return result;
+    }
+    while x > 10 {
+        result.push(x % 10);
+        x = x / 10;
+    }
+    result.push(x);
+    result
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_making_digits() {
+        assert_eq!(
+            make_digits(123).sort_unstable(),
+            vec![1, 2, 3].sort_unstable()
+        );
+    }
+
+    #[test]
+    fn test_is_divisible() {
+        assert_eq!(is_evenly_divisible(10, 2), true);
+        assert_eq!(is_evenly_divisible(11, 2), false);
+        assert_eq!(is_evenly_divisible(111, 1), true);
+        assert_eq!(is_evenly_divisible(20, 0), false);
+    }
+
+    #[test]
+    fn test_run() {
+        assert_eq!(run(244), Ok(true));
+        assert_eq!(run(211), Ok(false));
+        assert_eq!(run(0), Err("This doesn't work with zero!"));
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+extern crate digit_divisible;
+use digit_divisible::*;
+use std::process;
+
+fn main() {
+    let num = 0;
+
+    match run(num) {
+        Err(e) => {
+            eprintln!("Error: \n{}", e);
+            process::exit(1);
+        }
+        Ok(r) => {
+            match r {
+                true => println!("{:?} is divisible by all its digits", num),
+                false => println!("{:?} is not divisible by all its digits", num),
+            }
+        }
+    }
+}
